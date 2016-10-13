ee1fb6f736e808646f25ddb479028d39e3acfb17
helicopterito <ichaos@hotmail.es>
2016-10-13 10:42:10 -0500

    Arreglos dinámicos

--- primerExamen/src/codigo/Biblioteca.java
+++ primerExamen/src/codigo/Biblioteca.java
@@ -4,59 +4,52 @@ import java.util.*;
 
 public class Biblioteca {
 
-
-    public Biblioteca() {
-    }
-
-
     private ArrayList<Socio> sociosNoFiables;
     private ArrayList<Socio> socios;
     private ArrayList<Bibliotecario> bibliotecarios;
     private ArrayList<Libro> libros;
     private ArrayList<Prestamo> prestamos;
+    private String nombre;
+    private String direccion;
 
-
-
-
-
-
-
+    public Biblioteca() {
+        this.sociosNoFiables = null;
+        this.socios = null;
+        this.bibliotecarios = null;
+        this.libros = null;
+        this.prestamos = null;
+        this.nombre = "";
+        this.direccion = "";
+    }
 
     public void darDeAltaSocio(Socio socio) {
         this.socios.add(socio);
-        
-    }
 
+    }
 
     public void multarSocio(Socio socio) {
-        if(socio.getNumeroDeLibrosEnPrestamo() > 5)
-        {
+        if (socio.getNumeroDeLibrosEnPrestamo() > 5) {
             this.sociosNoFiables.add(socio);
         }
-        
+
     }
-    
-    public void prestarLibro(Socio socio, Libro libro)
-    {
-        for(Socio s : this.sociosNoFiables)
-        {
-            if(s.getNumeroDeSocio().equalsIgnoreCase(socio.getNumeroDeSocio()))
-            {
+
+    public void prestarLibro(Socio socio, Libro libro) {
+        for (Socio s : this.sociosNoFiables) {
+            if (s.getNumeroDeSocio().equalsIgnoreCase(socio.getNumeroDeSocio())) {
                 System.out.println("No se puede prestar el libro");
                 break;
             }
         }
-        
-        Date d1 = new Date();
-        Date d2 = new Date(d1.getTime() * 60 *60 *24 *5);
-        Prestamo p1 = new Prestamo(d1, d2, libro.getCodigo(), socio.getNumeroDeSocio());
+
+
+        Prestamo p1 = new Prestamo(libro, socio);
         this.prestamos.add(p1);
     }
 
-
     public void adquirirLibro() {
         // TODO implement here
-        
+
     }
 
     public ArrayList<Socio> getSociosNoFiables() {
@@ -97,6 +90,22 @@ public class Biblioteca {
 
     public void setPrestamos(ArrayList<Prestamo> prestamos) {
         this.prestamos = prestamos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
 
 }
