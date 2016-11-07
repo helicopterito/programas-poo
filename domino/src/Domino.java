a12c2818dcd6b41829c015093c991b6a3b0872f7
Néstor A. M <alvarado.nestor@yandex.com>
2016-11-06 22:11:11 -0600

    Fixed bug que no quitaba la mula de 6

--- domino/src/Domino.java
+++ domino/src/Domino.java
@@ -1,5 +1,4 @@
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,14 +6,11 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Néstor Alvarado Moreno
@@ -26,72 +22,53 @@ public class Domino {
     ArrayList<Ficha> fichasTiradas;
     int turno;
     boolean hayGanador;
-    
-    public void iniciarJuego()
-    {
+
+    public Domino() {
+        this.fichasDeJuego = new ArrayList();
+        this.jugadores = new ArrayList();
+        this.fichasTiradas = new ArrayList();
+        this.turno = 0;
+        this.hayGanador = false;
+
+    }
+
+    public void iniciarJuego() {
         crearSopa();
         repartirAlJugador();
     }
-    
-    public void repartirFichas()
-    {
-        
+
+    public void repartirAlJugador() {
         Collections.shuffle(fichasDeJuego);
-        
-        System.out.println("Fichas");
-        this.jugadores.get(0).fichasDeJugador.add(fichasDeJuego.get(0));
-        this.fichasDeJuego.remove(fichasDeJuego.get(0));
-        this.jugadores.get(1).fichasDeJugador.add(fichasDeJuego.get(0));       
-        this.fichasDeJuego.remove(fichasDeJuego.get(0));
-        this.jugadores.get(0).fichasDeJugador.add(fichasDeJuego.get(0));
-        this.fichasDeJuego.remove(fichasDeJuego.get(0));
-        this.jugadores.get(1).fichasDeJugador.add(fichasDeJuego.get(0));       
-        this.fichasDeJuego.remove(fichasDeJuego.get(0));
-        
-    }
-    
-    public void repartirAlJugador()
-    {
-        Collections.shuffle(fichasDeJuego);
-        for(Jugador j : this.jugadores)
-        {
+        for (Jugador j : this.jugadores) {
             for (int i = 0; i < 7; i++) {
                 j.fichasDeJugador.add(fichasDeJuego.get(0));
                 this.fichasDeJuego.remove(0);
-                
+
             }
         }
     }
-    
-    public void crearSopa()
-    {
-        
-        int limite=0;
-        for(int i=0;i<=6;i++)
-        { 
-            if(i>=1)
-            { 
+
+    public void crearSopa() {
+
+        int limite = 0;
+        for (int i = 0; i <= 6; i++) {
+            if (i >= 1) {
                 limite++;
             }
-            for(int j=6;j>=limite;j--)
-            {
-                fichasDeJuego.add(new Ficha(i,j));
+            for (int j = 6; j >= limite; j--) {
+                fichasDeJuego.add(new Ficha(i, j));
             }
         }
     }
-    
-    
 
-    public boolean  validarFicha(Ficha ficha, char car)
-    {
-        
+    public boolean validarFicha(Ficha ficha, char car) {
+
         switch (car) {
             case 'D':
                 if ((this.fichasTiradas.get(this.fichasTiradas.size() - 1).getLadoDerecho() == ficha.getLadoIzquierdo())
                         || (this.fichasTiradas.get(this.fichasTiradas.size() - 1).getLadoDerecho() == ficha.getLadoDerecho())
                         || (this.fichasTiradas.get(this.fichasTiradas.size() - 1).getLadoIzquierdo() == ficha.getLadoIzquierdo()
-                        || (this.fichasTiradas.get(this.fichasTiradas.size() - 1).getLadoIzquierdo() == ficha.getLadoDerecho()))
-                        ) {
+                        || (this.fichasTiradas.get(this.fichasTiradas.size() - 1).getLadoIzquierdo() == ficha.getLadoDerecho()))) {
                     return true;
                 } else {
                     return false;
@@ -105,28 +82,24 @@ public class Domino {
                 } else {
                     return false;
                 }
-                
+
             default:
                 return false;
         }
-        
+
     }
-    
-    
-    public int buscarMulaDeSeis()
-    {
+
+    public int buscarMulaDeSeis() {
         int posicion = 0;
         for (int i = 0; i < this.jugadores.size(); i++) {
-            for(Ficha f : this.jugadores.get(i).fichasDeJugador)
-            {
-                if(f.getLadoDerecho() == 6 && f.getLadoIzquierdo() == 6)
-                {
-                   posicion = i;
+            for (Ficha f : this.jugadores.get(i).fichasDeJugador) {
+                if (f.getLadoDerecho() == 6 && f.getLadoIzquierdo() == 6) {
+                    posicion = i;
                 }
             }
-            
+
         }
-        
+
         return posicion;
     }
 
@@ -144,6 +117,7 @@ public class Domino {
             if (this.fichasTiradas.size() == 0) {
                 try {
                     fichasTiradas.add(this.jugadores.get(turno).tirarFicha());
+                    this.jugadores.get(turno).fichasDeJugador.remove(this.fichasTiradas.get(0));
                     this.turno++;
                 } catch (IOException ex) {
                     Logger.getLogger(Domino.class.getName()).log(Level.SEVERE, null, ex);
@@ -202,31 +176,18 @@ public class Domino {
             }
 
         }
-        
+
         System.out.println("El jugador #" + (turno + 1) + " ha ganado");
     }
-    
-    public void imprimirFichasEnJuego()
-    {
+
+    public void imprimirFichasEnJuego() {
         System.out.println("Fichas del juego: ");
-        for(Ficha f : this.fichasTiradas)
-        {
+        for (Ficha f : this.fichasTiradas) {
             System.out.println(f.getLadoIzquierdo() + "|" + f.getLadoDerecho());
         }
-        
+
     }
-    
-    public Domino()
-    {
-        this.fichasDeJuego = new ArrayList();
-        this.jugadores = new ArrayList();
-        this.fichasTiradas = new ArrayList();
-        this.turno = 0;
-        this.hayGanador = false;
-        
-    }
-    
-    
+
     public static void main(String[] args) {
         Domino d1 = new Domino();
         Jugador j1 = new Jugador();
@@ -236,9 +197,8 @@ public class Domino {
         d1.jugadores.add(j1);
         d1.jugadores.add(j2);
         d1.jugadores.add(j3);
-        d1.jugadores.add(j4);        
+        d1.jugadores.add(j4);
         d1.comenzarJuego();
- 
-        
+
     }
 }
